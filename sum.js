ecd02b09c0a7f302c5aa455dc1b2ff6e936fc1a2
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-10-05 17:32:27 +0100

    Add string length

---
+++ sum.js
@@ -0,0 +1,15 @@
+
+const stringLength = (string) => {
+  return string.length;
+  // console.log(string.charAt(0));
+}
+module.exports = stringLength;
+
+stringLength('hi');
+stringLength('hey');
+stringLength('hello');
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// module.exports = sum;
---
+++ sum.test.js
@@ -0,0 +1,5 @@
+const stringLength = require('./sum');
+
+test('stringLength', () => {
+  expect(stringLength);
+});
